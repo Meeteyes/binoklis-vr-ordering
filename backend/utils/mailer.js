31d08dfab1db1a361d85eed0fbf2b0fc8ebef86c
Reinis <reinis@Reiniss-MacBook-Pro.local>
2022-02-16 14:57:05 +0100

    Changed the from address

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -16,7 +16,7 @@ export const main = async (email) => {
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: `Binoklis <siabinoklis@outlook.com>`, // sender address
+    from: process.env.EMAIL, // sender address
     to: email, // list of receivers
     subject: "We received your booking", // Subject line
     text: "Thank you very much for the booking. Someone form our company will get in touch with you in next 2 days", // plain text body
